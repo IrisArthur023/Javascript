11ed0c830e0d5ac6dacc619358b68834b56de0dc
IrisArthur023 <capman8976@gmail.com>
2026-05-06 16:43:40 +0000

    Javascript lesson

---
+++ Index.js
@@ -0,0 +1,27 @@
+let  x = 26
+
+let y = 2
+
+let z = x + y
+
+console.log((x+y/y*z)%5)
+
+
+const t = 3.14285714285714
+
+
+console.log(t.toFixed(6)*x)
+
+let age = 18 
+console.log(age >= 18)
+
+
+let a = 10
+let b = 5
+
+console.log(a > b && b < 3)
+
+let d = 8
+let y = 8
+
+console.log(d === y || d > 10)
