adc38526934d9c7aee2eef5c79a0b593d71e9095
IrisArthur023 <Capman8976@gmail.com>
2026-05-09 21:19:54 +0000

    Js Week 1 assignment

---
+++ Index.js
@@ -0,0 +1,10 @@
+// operators
+ let a = 6 
+ let b = 7
+
+ console.log(a + b)
+
+ let c = 30
+ let d = 14
+
+ console.log(++c%d)
---
+++ myscript.js
@@ -0,0 +1,63 @@
+//strings
+let item = "Article"
+console.log(item)
+
+let course = "science"
+console.log(course)
+
+let Animal = "dog"
+console.log(Animal)
+
+let mat = "rag"
+console.log()
+
+// operators
+ let a = 6 
+ let b = 7
+
+ console.log(a + b)
+
+ let c = 30
+ let d = 14
+
+ console.log(++c%d)
+
+  let e = 30
+ let f = 14
+
+ console.log(++e%f)
+
+  let g = 30
+ let h = 14
+
+ console.log(++g%h)
+
+ // floats 
+console.log(2.6,4.0)
+
+console.log(4.6,5.0)
+
+console.log(5.6,2.0)
+
+console.log(6.6,3.0)
+
+console.log(7.6,4.0)
+
+// booleans
+let isRaining = true
+console.log(isRaining)
+
+
+let isSunny = false
+console.log(isSunny)
+
+let isOnline = true;
+console.log(isOnline);
+
+let isOffline = false;
+console.log(isOffline);
+
+let loggedIn = true;
+let hasAccess = false;
+
+console.log(loggedIn && hasAccess);
