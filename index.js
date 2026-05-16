3d9dcdff00ed40816e74018698c3f5ab615598d1
IrisArthur023 <capman8976@gmail.com>
2026-05-16 16:01:15 +0000

    Week 3 updates
    JavaScript week 3 lessons.

---
+++ index.js
@@ -0,0 +1,33 @@
+let shirt = ["polo","Off-white","Prada","Dochi & Devana"]
+console.log(shirt[1])
+
+//IF ELSE STATEMENT
+let weather = "Stormy"
+if (weather === "Rainy"){
+    console.log("Drive To work")
+} else if (weather === "Stormy"){
+    console.log("Stay at home")
+}else{
+    console.log("Go to work")
+}
+
+
+// FOR LOOP
+let Number = 100
+for ( 
+    let x = 0;
+    x <= 99;
+    x++ 
+){
+    console.log("Number:" +x)
+}
+
+let count = 1;
+while(count<=30){
+    console.log("count:" + count)
+    count++
+}
+
+let student = ["patrick", 90 , "very single","guava"]
+console.log(student[2]);
+
